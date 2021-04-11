a5192623cfa46cf2e9aa7d536819496a7f416512
jennybolena <bolenajenny22@gmail.com>
2021-04-11 03:06:18 +0300

    fix case a date

--- Exercise7/script.js
+++ Exercise7/script.js
@@ -64,27 +64,27 @@ function getDailyFact(){
 	var current_day = current_date.getDay();
 	var el = document.querySelector('#fun_fact');
 	el.innerHTML = current_day;
-
+	
 	switch(current_day) {
-	  case 1:
-	    el.innerHTML = 'SQL Server is one of the market leaders in database technology';
+	  case 0:
+	    el.innerHTML = 'SQL Server is one of the market leaders in database technology.';
 	    break;
-	  case 2:
+	  case 1:
 	      el.innerHTML = "SQL Server's main competitors are Oracle and PostgreSQL.";
 	      break;
-	  case 3:
+	  case 2:
 	    el.innerHTML = 'SQL was initially developed at IBM.';
 	    break;
-	  case 4:
+	  case 3:
 	    el.innerHTML = 'SQL is Turing complete.';
 	    break;
-	  case 5:
+	  case 4:
 	    el.innerHTML = 'SQL is a little more than 50 years old.';
 	    break;
-	  case 6:
+	  case 5:
 	    el.innerHTML = 'The last stable release of the SQL Server was in <em>4 November 2019</em>.';
 	    break;
-	  case 7:
+	  case 6:
 	    el.innerHTML = 'SQl Server is wirtten in the programmig languages <em>C</em> and <em>C++</em>';
 	}
 }

c7a3579b8468cf9c7a0bbb98fe2ab62771557766
jennybolena <bolenajenny22@gmail.com>
2021-04-12 20:57:28 +0300

    add password show

--- Exercise8/form.js
+++ Exercise8/form.js
@@ -2,3 +2,21 @@
 function validateData() {
     console.log("ok");
 }
+
+
+function changePasswordDisplay(el){
+    let psw = document.getElementById('password');
+    let val_psw = document.getElementById('validate_password');
+    if(el.checked == true){
+        psw.type = 'text';
+        val_psw.type ='text';
+    }else{
+        psw.type = 'password';
+        val_psw.type = 'password';
+    }
+}
+
+function displayTimeSpent(el) {
+    el.nextElementSibling.value = el.value;
+}
+

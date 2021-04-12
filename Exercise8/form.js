728dd3edb746cf106c448f55dd2c83bcc01daaea
jennybolena <bolenajenny22@gmail.com>
2021-04-13 02:24:41 +0300

    add calculate age function

--- Exercise8/form.js
+++ Exercise8/form.js
@@ -1,9 +1,3 @@
-
-function validateData() {
-    console.log("ok");
-}
-
-
 function changePasswordDisplay(el){
     let psw = document.getElementById('password');
     let val_psw = document.getElementById('validate_password');
@@ -20,3 +14,30 @@ function displayTimeSpent(el) {
     el.nextElementSibling.value = el.value;
 }
 
+function setMaxDate(){
+    let el = document.getElementById('birthday');
+    el.max =new Date().toISOString().split("T")[0];
+}
+
+function calculateAge() {
+    let today = new Date(document.getElementById('birthday').max);
+    let birthday = new Date(document.getElementById('birthday').value);
+
+    let utc1 = Date.UTC(today.getFullYear(), today.getMonth(), today.getDate());
+    let utc2 = Date.UTC(birthday.getFullYear(), birthday.getMonth(), birthday.getDate());
+
+    let oneDay = 24 * 60 * 60 * 1000; //in ms
+    let age = Math.floor(Math.floor((utc1 - utc2) / oneDay) / 365);
+    return age;
+
+}
+
+function validateData() {
+    if(calculateAge() < 18){
+       return false;
+    }
+
+   return true;
+}
+
+//alert(document.querySelector('input[name="start_learning"]:checked').value);
